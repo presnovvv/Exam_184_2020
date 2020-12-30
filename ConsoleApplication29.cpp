be55a508b23ef7f38ffa99a9af65586d03cec9b9
Дмитрий Преснов <71821696+presnovvv@users.noreply.github.com>
2020-12-30 11:24:27 +0300

    EXAM

---
+++ ConsoleApplication29.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+void stroka(string f, int o)
+{
+	ofstream out;
+	out.open(f);
+	if (out.is_open())
+	{
+		for (int i = 1; i < o; i++) {
+			out << "" << i << endl;
+		}
+		out << "I like codding!" << endl;
+	}
+
+	cout << "printed on line " << o << endl;
+	cout << "End of program" << endl;
+	out.open("D:\\hello.txt");
+
+}
+int main()
+{
+	int line;
+	cout << "Enter the line : ";
+	cin >> line;
+	string e = "D\\:hello.txt";
+	stroka(e,line);
+	
+	return 0;
+}
